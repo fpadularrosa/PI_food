d83d074192b86b361389e811e6a557f540febdf8
unknown <francofyzz15@gmail.com>
2022-03-01 10:16:15 -0300

    commit pi

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App';
+import axios from 'axios';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import dotenv from 'dotenv';
+dotenv.config();
+
+axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001';
 
 ReactDOM.render(
   <React.StrictMode>
--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -7,57 +7,95 @@ import {
     FILTER_RECIPES,
     ALPHABETICAL_SORT,
     SCORE_SORT
-  } from "./constants";
-
-                    //arrow recibe name, devuelve arrow que recibe dispatch
+} from "./constants";
+import axios from 'axios';
 
 export function getRecipes(){
-    return function(dispatch){
-            fetch('http://localhost:3001/recipes')
-            .then(recipes => recipes.json())
-            .then(recipes => dispatch({ type: GET_RECIPES, payload: recipes}))
-            .catch(err => dispatch({ type: GET_RECIPES, payload:  err.toString()}))
+    return async function(dispatch){
+        try {
+            const recipes = await axios.get('http://localhost:3001/recipes');
+            if(recipes?.data){
+                dispatch({ type: GET_RECIPES, payload: recipes.data})
+            }
+        } catch (err) {
+            dispatch({ type: GET_RECIPES, payload:  err.toString()})
+        }
+        // .then(recipes => recipes.json())
+        // .then(recipes => dispatch({ type: GET_RECIPES, payload: recipes}))
+        // .catch(err => dispatch({ type: GET_RECIPES, payload:  err.toString()}))
     }
 }
 
 export function getDetails(id) {
-    return function (dispatch) {
-            fetch(`http://localhost:3001/recipes/${id}`)
-            .then(details => details.json())
-            .then(details => dispatch({ type: GET_DETAIL, payload: details }))
-            .catch(err => dispatch({ type: GET_DETAIL, payload:  err.toString()}))
+    return async function (dispatch) {
+        try {
+            const details = await axios.get(`http://localhost:3001/recipes/${id}`);
+            if(details?.data){
+                dispatch({ type: GET_DETAIL, payload: details.data})
+            }
+        } catch (err) {
+            dispatch({ type: GET_DETAIL, payload:  err.toString()})
+        }
+            // fetch(`http://localhost:3001/recipes/${id}`)
+            // .then(details => details.json())
+            // .then(details => dispatch({ type: GET_DETAIL, payload: details }))
+            // .catch(err => dispatch({ type: GET_DETAIL, payload:  err.toString()}))
     }
 }
 
 export function getDiets() {
-    return function (dispatch) {
-        fetch('http://localhost:3001/types')
-        .then(diets => diets.json())
-        .then(diets => dispatch({ type: GET_DIETS, payload: diets }))
-        .catch(err => dispatch({ type: GET_DIETS, payload:  err.toString()}))
+    return async function (dispatch) {
+        try {
+            const diets = await axios.get('http://localhost:3001/types');
+            if(diets?.data){
+                dispatch({type: GET_DIETS, payload: diets.data})
+            }
+        } catch (err) {
+            dispatch({type: GET_DIETS, payload: err.toString()})
+        }
+        // fetch('http://localhost:3001/types')
+        // .then(diets => diets.json())
+        // .then(diets => dispatch({ type: GET_DIETS, payload: diets }))
+        // .catch(err => dispatch({ type: GET_DIETS, payload:  err.toString()}))
     }
 }
 
 export function postRecipe(input) {
     return async function (dispatch) {
-            fetch("http://localhost:3001/recipe", {
-                method: "POST",
-                body: JSON.stringify(input),
-                headers:{'Content-Type':'application/json'}
-            })
-            .then(newRecipe => newRecipe.json())
-            .then(newRecipe => dispatch({ type: POST_RECIPE, payload: newRecipe }))
-            .catch(err => dispatch({ type: POST_RECIPE, payload:  err.toString()}))
+        try {
+            const newRecipe = await axios.post("http://localhost:3001/recipe", input, {
+                headers:{'content-type':'application/json'}});
+                if(newRecipe?.data){
+                    dispatch({ type: POST_RECIPE, payload: newRecipe.data });
+                }
+        } catch (err) {
+            dispatch({ type: POST_RECIPE, payload:  err.toString()})
+        }
+
+            // fetch("http://localhost:3001/recipe", {
+            //     method: "POST",
+            //     body: JSON.stringify(input),
+            //     headers:{'Content-Type':'application/json'}
+            // })
+            // .then(newRecipe => newRecipe.json())
+            // .then(newRecipe => dispatch({ type: POST_RECIPE, payload: newRecipe }))
+            // .catch(err => dispatch({ type: POST_RECIPE, payload:  err.toString()}))
     }
 }
 
-export const getRecipesByName = name => dispatch => {
-    return (
-        fetch(`http://localhost:3001/recipes?name=${name}`)
-        .then(recipe => recipe.json())
-        .then(recipe => dispatch({type: GET_RECIPES_BY_NAME, payload: recipe}))
-        .catch(err => dispatch({type: GET_RECIPES_BY_NAME, payload: err.toString()}))
-    )
+export const getRecipesByName = name => async dispatch => {
+        try {
+            const recipesByName = await axios.get(`http://localhost:3001/recipes?name=${name}`);
+            if(recipesByName?.data){
+                dispatch({type: GET_RECIPES_BY_NAME, payload: recipesByName.data});
+            }
+        } catch (err) {
+            dispatch({type: GET_RECIPES_BY_NAME, payload: err.toString()});
+        }
+        // fetch(`http://localhost:3001/recipes?name=${name}`)
+        // .then(recipe => recipe.json())
+        // .then(recipe => dispatch({type: GET_RECIPES_BY_NAME, payload: recipe}))
+        // .catch(err => dispatch({type: GET_RECIPES_BY_NAME, payload: err.toString()}))
 }
 
 export const filterBy = diet => {
